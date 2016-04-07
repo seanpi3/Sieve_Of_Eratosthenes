9ac73d3ed721252d978ca30244a1bfe3bffcf119
sean <sean@debian>
2016-04-07 19:27:05 -0400

    seg fault

--- sieveOfEratosthenes.c
+++ sieveOfEratosthenes.c
@@ -5,7 +5,8 @@
 pthread_cond_t cond;
 pthread_mutex_t mutex;
 pthread_mutex_t ready_queue_mutex;
-int work = 0;
+pthread_barrier_t init_barrier;
+int* marked;
 
 typedef struct ready_queue{
 	pthread_t id;
@@ -15,26 +16,43 @@ typedef struct ready_queue{
 struct ready_queue *head;
 struct ready_queue *tail;
 
-typedef struct numbers{
-	int number;
-	int marked;
+typedef struct work{
+	int lower_bound;
+	int upper_bound;
+	int k;
 };
-struct numbers** number_array;
+
+struct work **jobs;
+
+typedef struct slave{
+	int slave_num;
+	pthread_t id;
+};
+
+struct slave **slaves;
 
 /*Logic for the slave threads*/
 void *slaveLogic(void *arg){
 	int ready;
-	pthread_t id = (pthread_t)arg;
+	struct slave *slave_info = (struct slave*)arg;
+
+	printf("Thread: %d waiting for all threads to be ready\n",slave_info->slave_num);
+	pthread_barrier_wait(&init_barrier);
 
 	/*Add self to the ready queue*/
 	pthread_mutex_lock(&ready_queue_mutex);
 	tail->next = malloc(sizeof(struct ready_queue));
 	tail = tail->next;
-	tail->id = id;
+	tail->id = slave_info->id;
 	pthread_mutex_unlock(&ready_queue_mutex);
 
-
+	printf("Yo im ready dawg\n");
 	return 0;
+}
+
+
+void assign_work(int k, int chunk_size){
+	
 }
 
 int main(int argc, char *argv[]){
@@ -51,25 +69,35 @@ int main(int argc, char *argv[]){
 		chunk_size=atoi(argv[3]);
 	}
 	
+	pthread_barrier_init(&init_barrier,NULL,num_threads);
 
-	/* Initialize the queue*/
-	pthread_t slaves[num_threads];
+	/* Initialize the slaves*/
+	slaves = malloc(num_threads);
+	for(i=0;i<num_threads;i++){
+					slaves[i] = malloc(sizeof(struct slave));
+	}
 	tail = malloc(sizeof(struct ready_queue));
 	head = malloc(sizeof(struct ready_queue));
 	tail->next = head;
 	
 	for(i=0;i<num_threads;i++){
-		pthread_create(&slaves[i],NULL,slaveLogic,(void*)slaves[i]);
+		if(i==4) printf("seg fault inc\n");
+		pthread_create(&slaves[i]->id,NULL,slaveLogic,(void*)slaves[i]);
+		slaves[i]->slave_num = i;
 	}
+
+	printf("I got here \n");
+
 	
 	/*Initialize the numbers*/
-	number_array = malloc(sizeof(struct numbers)*(n-1));
-	for(i=0;i<0;i++){
-		number_array[i]->number = i+1;
-		number_array[i]->marked = 0;
-	}
-
-
+	marked = malloc(sizeof(int)*(n+1));
+	for(i=0;i<n+1;i++)
+		marked[i] = 0;
+	
+	/*Initialize work*/
+	jobs = malloc(num_threads);
+	for(i=0;i<num_threads;i++)
+		jobs[i] = malloc(sizeof(struct work));
 
 
 
